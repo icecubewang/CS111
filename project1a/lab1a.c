66011b75f775d6a3782712a2e0006de84643d915
icecubewang <wangfeilan@s-169-232-235-210.resnet.ucla.edu>
2018-04-12 23:02:54 -0700

    able to parse argument

--- project1a/lab1a.c
+++ project1a/lab1a.c
@@ -7,6 +7,8 @@
 #include <strings.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
 
 static struct termios terminal_old;
 static struct termios terminal_new;
@@ -22,8 +24,23 @@ void reset_input_mode(void) {
 }
 
 int main(int argc, char *argv[]) {
+
+	//Parse arguments
+	int c = 0;
+	bool shell = false;
+	struct option long_options[] = {
+		{"shell", no_argument, 0, 's'},
+		{0, 0, 0, 0}
+	};
+	int option_index = 0;
+	c = getopt_long(argc, argv, "s", long_options, &option_index);
+	if (c == 's') {
+		shell = true;
+	}
+
+	//Declare vairables
 	int res;
-	char buf[255];
+	char buf[256];
 
 	//Obtain terminal settings (save current port setting)
 	if (tcgetattr(terminal_descriptor, &terminal_old))
@@ -46,7 +63,7 @@ int main(int argc, char *argv[]) {
 	tcsetattr(terminal_descriptor, TCSANOW, &terminal_new);	//TCSANOW: make the change immediately
 
 	do {
-		res = read(terminal_descriptor, &buf, 255);
+		res = read(terminal_descriptor, &buf, 256);
 		if (buf[0] == '\r' || buf[0] == '\n') {	//'\013' == /r and '\010' == /n
 			buf[0] = '\r';
 			buf[1] = '\n';
