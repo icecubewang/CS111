a395cc51379b8a9523871b7444dcd16aa1b8a700
icecubewang <wangfeilan@hotmail.com>
2018-05-05 18:34:07 -0700

    implemented yield and it works

--- project2a/lab2_add.c
+++ project2a/lab2_add.c
@@ -7,23 +7,26 @@
 #include <errno.h>
 #include <time.h>		/* for clock_gettime */
 #include <pthread.h>
+#include <sched.h> 		/* for sched_yield */
 
 /* Global variables */
 int noOfThreads = 1;
 int noOfIterations = 1;
+int opt_yield = 0;
 
 /* Check return value function */
 void check_return_value(int ret, int errnum) {
 	if (ret < 0) {
 		fprintf(stderr, "Error: %s\n", strerror(errnum));
-		//If any errors are encountered, exit with a return code of 2. 
-		exit(2);
+		exit(1);
 	}
 }
 
 /* Add function */
 void add(long long *pointer, long long value) {
 	long long sum = *pointer + value;
+	if (opt_yield)
+		sched_yield();
 	*pointer = sum;
 }
 
@@ -44,6 +47,7 @@ int main(int argc, char *argv[]) {
 	struct option long_options[] = {
 		{"threads", required_argument, 0, 't'},
 		{"iterations", required_argument, 0, 'i'},
+		{"yield", no_argument, 0, 'y'},
 		{0, 0, 0, 0}
 	};
 	int option_index = 0;
@@ -54,6 +58,9 @@ int main(int argc, char *argv[]) {
 				break;
 			case 'i':
 				noOfIterations = atoi(optarg);
+				break;
+			case 'y':
+				opt_yield = 1;
 				break;
 			default:
 				fprintf(stderr, "Usage: ./lab2_add --threads=[number] --iterations=[number]\n");
@@ -92,7 +99,14 @@ int main(int argc, char *argv[]) {
 	int timePerOps = elapsedTime / noOfOps;
 	char msg[1000];
 	printf("%d\n", counter);
-	sprintf(msg, "add-none,%d,%d,%d,%d,%d,%d\n", noOfThreads, noOfIterations, noOfOps, elapsedTime, timePerOps, counter);
+	char* begin;
+	if (opt_yield) {
+		begin = "add-yield";
+	}
+	else {
+		begin = "add-none";
+	}
+	sprintf(msg, "%s,%d,%d,%d,%d,%d,%d\n", begin, noOfThreads, noOfIterations, noOfOps, elapsedTime, timePerOps, counter);
 	int ret;
 	ret = write(STDOUT_FILENO, msg, strlen(msg));
 	int errnum = errno;
