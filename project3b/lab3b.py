4426b15de04f50a796425b60dd86aa8e1999db2c
icecubewang <wangfeilan@hotmail.com>
2018-06-02 00:11:47 -0700

    modify parse argument

--- project3b/lab3b.py
+++ project3b/lab3b.py
@@ -2,9 +2,20 @@
 #EMAIL: wangfeilan@hotmail.com, kana0111@g.ucla.edu
 #ID: 104796844, 404849285
 
-#heello
 #!/user/local/cs/bin/python
+from __future__ import print_function
+import csv
 import sys
+
+#initialization
+blockDict_Isfree = dict()
+blockDict_allcation = dict()
+
+inodeDict_allocated = dict()
+inodeDict_linkCount = dict()
+inodeDict_realParent = dict()
+inodeDict_ReferenceNumber = dict()
+inodeDict_children = dict()
 
 #container
 class superBlockInfo:
@@ -49,38 +60,38 @@ def parseArgument():
 		for line in f:
 			x = line.split(",")
 			if x[0] = "SUPERBLOCK":
-				totalNumberOfBlocks = x[1]
-				totalNumberOfInodes = x[2]
-				blockSize = x[3]
-				inodeSize = x[4]
-				blocksPerGroup = x[5]
-				inodesPerGroup = x[6]
-				firstNonReservedInode = x[7]
+				totalNumberOfBlocks = int(x[1])
+				totalNumberOfInodes = int(x[2])
+				blockSize = int(x[3])
+				inodeSize = int(x[4])
+				blocksPerGroup = int(x[5])
+				inodesPerGroup = int(x[6])
+				firstNonReservedInode = int(x[7])
 				mySuperBlock = superBlockInfo(totalNumberOfBlocks, totalNumberOfInodes, blockSize, inodeSize, blocksPerGroup, inodesPerGroup, firstNonReservedInode)
 				initContainer()
 
 			elif x[0] = "GROUP":
-				groupNumber = x[1]
-				totalNumberOfBlocks = x[2]
-				totalNumberOfInodes = x[3]
-				numberOfFreeBlocks = x[4]
-				numberOfFreeInodes = x[5]
-				freeBlockBitmap = x[6]
-				freeInodeBitmap = x[7]
-				firstBlockOfInodes = x[8]
+				groupNumber = int(x[1])
+				totalNumberOfBlocks = int(x[2])
+				totalNumberOfInodes = int(x[3])
+				numberOfFreeBlocks = int(x[4])
+				numberOfFreeInodes = int(x[5])
+				freeBlockBitmap = int(x[6])
+				freeInodeBitmap = int(x[7])
+				firstBlockOfInodes = int(x[8])
 
 			elif x[0] = "BFREE":
-				blockNumber = x[1]
+				blockNumber = int(x[1])
 				blockDict_Isfree[blockNumber] = True	#true == free, false == not free
 
 
 			elif x[0] = "IFREE":
-				inodeNumber = x[1]
+				inodeNumber = int(x[1])
 				inodeDict_Isfree[inodeNumber] = True
 
 			elif x[0] = "INDIRECT":
-				blockNumber = x[5]
-				myBlock = blockInfo(blockNumber, x[1], x[3], x[2])
+				blockNumber = int(x[5])
+				myBlock = blockInfo(blockNumber, int(x[1]), int(x[3]), int(x[2]))
 				if blockNumber not in blockDict_allcation:
 					blockDict_allcation[blockNumber] = set()
 				blockDict_allcation[blockNumber].add(myBlock)
@@ -88,7 +99,7 @@ def parseArgument():
 			elif x[0] = "INODE":
 				inodeNumber = x[1]
 				inodeDict_allocated[inodeNumber] = True
-				inodeDict_linkCount[inodeNumber] = x[6]
+				inodeDict_linkCount[inodeNumber] = int(x[6])
 
 			elif x[0] = "DIRENT":
 				inodeNumber = x[3]
@@ -97,10 +108,10 @@ def parseArgument():
 				else:
 					inodeDict_ReferenceNumber[inodeNumber] = 1
 
-				inodeDict_children[x[1]].append((x[6], x[3]))
+				inodeDict_children[x[1]].append((int(x[6]), int(x[3])))
 
 				#TODO: Can a file have two real parent?
-				inodeDict_realParent[inodeNumber] = x[1]
+				inodeDict_realParent[inodeNumber] = int(x[1])
 
 def initContainer():
 	for num in range(0, mySuperBlock.totalNumberOfBlocks):
